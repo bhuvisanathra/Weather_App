215411ef3a00adb6e915bc91d31d4623a6357caa
Bhuvnesh Sanathara <68009290+bhuvisanathra@users.noreply.github.com>
2023-08-09 23:09:21 +0530

    Added!!

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "b790b19e490ba8cead05002608a28600",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
